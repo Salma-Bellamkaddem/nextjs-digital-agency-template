41b3f19defaf1cae5482c9c24ba5d08ba2626648
MAC <salmabellamkaddem11@gmail.com>
2026-09-09 17:38:14 +0100

    version9

--- src/app/[locale]/pricing/page.tsx
+++ src/app/[locale]/pricing/page.tsx
@@ -1,9 +1,8 @@
 import { JSX } from 'react'
-import dynamic from 'next/dynamic'
 
-const PricingPage = dynamic(() => import('../_components/PricingPage'), {
-  
-})
+import PricingPage from '@/app/_components/PricingPage'
+
+
 
 export default function PricingRoute(): JSX.Element {
   return <PricingPage />
